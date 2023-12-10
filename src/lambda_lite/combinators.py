74146bbbfa9ec3d58c5e4f72811182fc1cf2ff57
lepmt001 <104551434+Michael-LePera@users.noreply.github.com>
2023-12-10 17:33:27 +1030

    Add tooling

--- src/lambda_lite/combinators.py
+++ src/lambda_lite/combinators.py
@@ -1,11 +1,9 @@
 from typing import Callable
+
 from .typedefs import *
 
-def compose[**A, **B, C](
-        f: ParametricFunction[B, C], 
-        g: ParametricFunction[A, B]
-    ) -> ParametricFunction[A, C]:
-    
+
+def compose[**A, **B, C](f: ParametricFunction[B, C], g: ParametricFunction[A, B]) -> ParametricFunction[A, C]:
     def h(*args: A.args, **kwargs: A.kwargs) -> C:
         if args or kwargs:
             return f(g(*args, **kwargs))
@@ -14,36 +12,66 @@ def compose[**A, **B, C](
     return h
 
 
-def flip[A, B, C](f: Callable[[A, B], C]) -> Callable[[B, A], C]:
+def flip_[A, B, C](f: Callable[[A, B], C]) -> Callable[[B, A], C]:
     def flipped(b: B, a: A) -> C:
         return f(a, b)
-    return flipped 
+
+    return flipped
 
 
-def identity[A](x: A) -> A:
+def id_[A](x: A) -> A:
+    """Identity combinator.
+
+    λa.a
+
+    """
     return x
 
-def const[A, B](a: A, b: B) -> A:
+
+def const_[A, B](a: A, b: B) -> A:
+    """K combinator.
+
+    λab.a
+
+    """
     return a
 
-def join[A, B](f: Callable[[A, A], B]) -> Callable[[A], B]:
+
+def join_[A, B](f: Callable[[A, A], B]) -> Callable[[A], B]:
     def inner(a: A) -> B:
         return f(a, a)
+
     return inner
+
 
 def s[A, B, C](f: Callable[[A, B], C], g: Callable[[A], B]) -> C:
     def inner(x: A) -> C:
         return f(x, g(x))
+
     return inner
 
 
-
-__all__ = [
-    "compose", 
-    "flip", 
-    "identity", 
-    "join", 
-    "s"
-]
+def on_[A, B, C](f: Callable[[B, B], C], g: Callable[[A], B], a: A, b: A) -> C:
+    """psi combinator"""
+    return f(g(a), g(b))
 
 
+def applicator[A, B](f: Callable[[A], B], a: A) -> B:
+    """i-star combinator"""
+    return f(a)
+
+
+def becard_[
+    A, B, C, D
+](f: Callable[[C], D], g: Callable[[B], C], h: Callable[[A], B], a: A) -> D:
+    """B3 combinator"""
+    return f(g(h(a)))
+
+
+def blackbird_[
+    A, B, C, D
+](f: Callable[[C], D], g: Callable[[A, B], C], a: A, b: B) -> D:
+    return f(g(a, b))
+
+
+__all__ = ["compose", "flip_", "id_", "join_", "s"]
--- src/lambda_lite/typedefs.py
+++ src/lambda_lite/typedefs.py
@@ -3,6 +3,18 @@ from typing import Callable
 type NiladicFunction[B] = Callable[[], B]
 type MonadicFunction[A, B] = Callable[[A], B]
 type DyadicFunction[A, B, C] = Callable[[A, B], C]
-type ParametricFunction[**A, B] = Callable[A, B]
+type ParametricFunction[**P, B] = Callable[P, B]
 type Predicate = Callable[..., bool]
+type VariadicFunction[B] = Callable[..., B]
+
+
+__all__ = [
+    "NiladicFunction",
+    "MonadicFunction",
+    "DyadicFunction",
+    "ParametricFunction",
+    "Predicate",
+    "VariadicFunction",
+
+]
 
